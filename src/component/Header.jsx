55154ab9ed869ef82fa461295f209bb80eaaad01
FTMFR <fatemefarajzade018@gmail.com>
2023-12-27 21:14:14 +0330

    add some components

---
+++ src/component/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return <h1 id="title">I am header section</h1>;
+};
+
+export default Header;
---
+++ src/component/Media.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./media.css";
+
+const Media = () => {
+  return (
+    <ul>
+      <li>Telegram</li>
+      <li>Twitter</li>
+      <li>Instagram</li>
+    </ul>
+  );
+};
+
+export default Media;
